1cf18096e11e0fc3be9e23b4d2c895a9f4c27c6a
toshihirokato <nanamin.104@icloud.com>
2021-05-07 23:23:26 +0900

    yarn analyzeコマンドの作成

--- nuxt.config.js
+++ nuxt.config.js
@@ -49,6 +49,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+    analyze: true,
     babel: {
       plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
     },
