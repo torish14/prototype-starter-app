d6eaa737ba421b51055f52cd72e6f613577ab27f
toshihirokato <nanamin.104@icloud.com>
2021-05-08 00:50:10 +0900

    エラーページの作成

---
+++ app/layouts/error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <h1 v-if="error.statusCode === 404">
+      音速で探しましたがページが見つかりません
+    </h1>
+    <h1 v-else>エラーが発生しました</h1>
+    <nuxt-link to="/">ホーム</nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null,
+    },
+  },
+}
+</script>
