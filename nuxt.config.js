76af087643fe5ea2e12e3eb001db1433c7cc109f
toshihirokato <nanamin.104@icloud.com>
2021-05-07 23:25:02 +0900

    ターミナルを見てごらん。

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,6 +24,10 @@ export default {
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
+  cli: {
+    badgeMessages: ['待たせたな。'],
+  },
+
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
     // https://go.nuxtjs.dev/typescript
