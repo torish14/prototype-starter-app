1d2f894ba7661a7b9762e71f943b501182d6fd57
toshihirokato <nanamin.104@icloud.com>
2021-05-07 22:49:12 +0900

    warn の削除

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,5 +47,9 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    babel: {
+      plugins: [['@babel/plugin-proposal-private-methods', { loose: true }]],
+    },
+  },
 }
